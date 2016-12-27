b9ce3abe8633e5127bea9c643efa23340efdad13
AkashS <iit2015067@iiita.ac.in>
2016-12-27 12:55:11 +0530

    Add scripts

---
+++ js/about.js
@@ -0,0 +1,5 @@
+document.getElementById("about-us").style.display = "none";
+document.getElementById("center-circle").onclick = function () {
+    document.getElementById('main_content').style.display = "none";
+    document.getElementById('about-us').style.display = "block";
+};
